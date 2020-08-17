fc5aa45a432425716e0f1305739d6372a96b81cd
YLyeliang <k87974@163.com>
2020-08-17 08:59:47 +0800

    update the AnchorGenerator

--- det/core/anchor/AnchorGenerator.py
+++ det/core/anchor/AnchorGenerator.py
@@ -52,6 +52,7 @@ class AnchorGenerator(object):
                 [11.5000, 11.5000, 20.5000, 20.5000]]), \
         tensor([[-9., -9., 9., 9.]])]
     """
+
     def __init__(self,
                  strides,
                  ratios,
@@ -63,18 +64,18 @@ class AnchorGenerator(object):
                  centers=None,
                  center_offset=0.):
         # check center and center_offset
-        if center_offset !=0:
+        if center_offset != 0:
             assert centers is None, 'center cannot be set when center_offset' \
                                     f'!=0, {centers} is given'
-        if not (0<=center_offset <=1):
+        if not (0 <= center_offset <= 1):
             raise ValueError('center_offset should be in range [0,1].'
                              f'{center_offset} is given.')
         if centers is not None:
-            assert len(centers)==len(strides), 'The number of strides should be the same as centers, got' \
-                                               f'{strides} and {centers}'
+            assert len(centers) == len(strides), 'The number of strides should be the same as centers, got' \
+                                                 f'{strides} and {centers}'
 
         # calculate base sizes of anchors
-        self.strides = [_pair(stride) for stride in strides]    #_pair is repeat(stride,2)
+        self.strides = [_pair(stride) for stride in strides]  # _pair is repeat(stride,2)
         self.base_sizes = [min(stride) for stride in self.strides] if base_sizes is None else base_sizes
         assert len(self.base_sizes) == len(self.strides), 'The number of strides should be the same as base sizes, got' \
                                                           f'{self.strides} and {self.base_sizes}'
@@ -86,7 +87,7 @@ class AnchorGenerator(object):
         if scales is not None:
             self.scales = torch.Tensor(scales)
         elif octave_base_scale is not None and scales_per_octave is not None:
-            octave_scales = np.array([2**(i/scales_per_octave) for i in range(scales_per_octave)])
+            octave_scales = np.array([2 ** (i / scales_per_octave) for i in range(scales_per_octave)])
             # above e.g. scales_per_octave=4, then 2^0 2^(1/4) 2^(1/2) 2^(3/4)
             scales = octave_base_scale * octave_scales
             self.scales = torch.Tensor(scales)
@@ -94,10 +95,10 @@ class AnchorGenerator(object):
             raise ValueError('Either scales or octave_base_scale with'
                              'scales_per_octave should be set')
 
-        self.octave_base_scale =octave_base_scale
-        self.scales_per_octave =scales_per_octave
+        self.octave_base_scale = octave_base_scale
+        self.scales_per_octave = scales_per_octave
         self.ratios = torch.Tensor(ratios)
-        self.scale_major =scale_major
+        self.scale_major = scale_major
         self.centers = centers
         self.center_offset = center_offset
         self.base_anchors = self.gen_base_anchors()
@@ -119,7 +120,7 @@ class AnchorGenerator(object):
             list(torch.Tensor): Base anchors of a feature grid in multiple \
             feature levels.
         """
-        multi_level_base_anchors=[]
+        multi_level_base_anchors = []
         for i, base_size in enumerate(self.base_sizes):
             center = None
             if self.centers is not None:
@@ -156,28 +157,28 @@ class AnchorGenerator(object):
             x_center = self.center_offset * w
             y_center = self.center_offset * h
         else:
-            x_center,y_center=center
+            x_center, y_center = center
 
         h_ratios = torch.sqrt(ratios)
-        w_ratios = 1/h_ratios
+        w_ratios = 1 / h_ratios
         if self.scale_major:
             # w_ratios is list. w_ratios[:,None] add a new dim, then become [n,1], scales [1,n] -> [n,n]
-            ws = (w*w_ratios[:,None]*scales[None,:]).view(-1)
-            hs = (h*h_ratios[:,None]*scales[None,:]).view(-1)
+            ws = (w * w_ratios[:, None] * scales[None, :]).view(-1)
+            hs = (h * h_ratios[:, None] * scales[None, :]).view(-1)
         else:
-            ws = (w*scales[:,None]*w_ratios[None,:]).view(-1)
-            hs = (h*scales[:,None]*h_ratios[None,:]).view(-1)
+            ws = (w * scales[:, None] * w_ratios[None, :]).view(-1)
+            hs = (h * scales[:, None] * h_ratios[None, :]).view(-1)
 
         # use float anchor and the anchor's center is aligned with the pixel center
         base_anchors = [
-            x_center - 0.5 *ws, y_center - 0.5*hs, x_center +0.5 * ws,
-            y_center +0.5 * hs
+            x_center - 0.5 * ws, y_center - 0.5 * hs, x_center + 0.5 * ws,
+            y_center + 0.5 * hs
         ]
-        base_anchors = torch.stack(base_anchors,dim=-1)
+        base_anchors = torch.stack(base_anchors, dim=-1)
 
         return base_anchors
 
-    def _meshgrid(self,x,y,row_major=True):
+    def _meshgrid(self, x, y, row_major=True):
         """
         Generate mesh grid of x and y.
         Args:
@@ -189,14 +190,14 @@ class AnchorGenerator(object):
         Returns:
             tuple[torch.Tensor]: The mesh grids of x and y.
         """
-        xx=x.repeat(len(y))
-        yy=y.view(-1,1).repeat(1,len(x)).view(-1)
+        xx = x.repeat(len(y))
+        yy = y.view(-1, 1).repeat(1, len(x)).view(-1)
         if row_major:
-            return xx,yy
+            return xx, yy
         else:
-            return yy,xx
+            return yy, xx
 
-    def grid_anchors(self,featmap_sizes,device='cuda'):
+    def grid_anchors(self, featmap_sizes, device='cuda'):
         """
         Generate grid anchors in multiple feature levels.
 
@@ -212,15 +213,21 @@ class AnchorGenerator(object):
                 are the sizes of the corresponding feature lavel, \
                 num_base_anchors is the number of anchors for that level.
         """
-        assert self.num_levels==len(featmap_sizes)
+        assert self.num_levels == len(featmap_sizes)
         multi_level_anchors = []
         for i in range(self.num_levels):
-            anchors = self
+            anchors = self.single_level_grid_anchors(
+                self.base_anchors[i].to(device),
+                featmap_sizes[i],
+                self.strides[i],
+                device=device)
+            multi_level_anchors.append(anchors)
+        return multi_level_anchors
 
     def single_level_grid_anchors(self,
                                   base_anchors,
                                   featmap_size,
-                                  stride=(16,16),
+                                  stride=(16, 16),
                                   device='cuda'):
         """
         Generate grid anchors of a single level.
@@ -239,13 +246,121 @@ class AnchorGenerator(object):
         Returns:
             torch.Tensor: Anchors in the overall feature maps.
         """
-        feat_h,feat_w=featmap_size
-        shift_x = torch.arange(0,feat_w,device=device)*stride[0]
-        shift_y = torch.arange(0,feat_h,device=device)*stride[1]
-        shift_xx,shift_yy = self._meshgrid(shift_x,shift_y)
-        shifts = torch.stack([shift_xx,shift_yy,shift_xx,shift_yy],dim=-1)
+        feat_h, feat_w = featmap_size
+        shift_x = torch.arange(0, feat_w, device=device) * stride[0]
+        shift_y = torch.arange(0, feat_h, device=device) * stride[1]
+        shift_xx, shift_yy = self._meshgrid(shift_x, shift_y)
+        shifts = torch.stack([shift_xx, shift_yy, shift_xx, shift_yy], dim=-1)
         shifts = shifts.type_as(base_anchors)
         # first feat_w elements correspond to the first row of shifts
         # add A anchors (1,A,4) to k shifts (k,1,4) to get
         # shifted anchors (k,A,4), reshape to (k*A,4)
 
+        all_anchors = base_anchors[None, :, :] + shifts[:, None, :]
+        all_anchors = all_anchors.view(-1, 4)
+        # first A rows correspond to A anchors of (0,0) in feature map,
+        # then (0, 1), (0, 2), ...
+        return all_anchors
+
+    def valid_flags(self, featmap_sizes, pad_shape, device='cuda'):
+        """
+        Generate valid flags of anchors in multiple feature levels.
+
+        Args:
+            featmap_sizes (list(tuple)): List of feature map sizes in
+                multiple feature levels.
+            pad_shape (tuple): The padded shape of the image.
+            device (str): Device where the anchors will be put on.
+
+        Return:
+            list(torch.Tensor): Valid flags of anchors in multiple levels.
+        """
+        assert self.num_levels == len(featmap_sizes)
+        multi_level_flags = []
+        for i in range(self.num_levels):
+            anchor_stride = self.strides[i]
+            feat_h, feat_w = featmap_sizes[i]
+            h, w = pad_shape[:2]
+            # min(feature map size, padded image size / stride)
+            valid_feat_h = min(int(np.ceil(h / anchor_stride[0])), feat_h)
+            valid_feat_w = min(int(np.ceil(w / anchor_stride[1])), feat_w)
+            flags = self.single_level_valid_flags((feat_h, feat_w),
+                                                  (valid_feat_h, valid_feat_w),
+                                                  self.num_base_anchors[i],
+                                                  device=device)
+
+    def single_level_valid_flags(self,
+                                 featmap_size,
+                                 valid_size,
+                                 num_base_anchors,
+                                 device='cuda'):
+        """
+        Generate the valid flags of anchor in a single feature map.
+
+        Args:
+            featmap_size (tuple[int]): The size of feature maps.
+            valid_size (tuple[int]): The valid size of the feature maps.
+            num_base_anchors (int): The number of base anchors.
+            device (str, optional): Device where the flags will be put on.
+                Defaults to 'cuda'.
+
+        Returns:
+            torch.Tensor: The valid flags of each anchor in a single level \
+                feature map.
+        """
+        feat_h, feat_w = featmap_size
+        valid_h, valid_w = valid_size
+        assert valid_h <= feat_h and valid_w <= feat_w
+        valid_x = torch.zeros(feat_w, dtype=torch.bool, device=device)
+        valid_y = torch.zeros(feat_h, dtype=torch.bool, device=device)
+        valid_x[:valid_w] = 1
+        valid_y[:valid_h] = 1
+        valid_xx, valid_yy = self._meshgrid(valid_x, valid_y)
+        valid = valid_xx & valid_yy
+        valid = valid[:, None].expand(valid.size(0), num_base_anchors).contiguous().view(-1)
+        return valid
+
+    def __repr__(self):
+        """str: a string that describes the module"""
+        indent_str = '    '
+        repr_str = self.__class__.__name__ + '(\n'
+        repr_str += f'{indent_str}strides={self.strides},\n'
+        repr_str += f'{indent_str}ratios={self.ratios},\n'
+        repr_str += f'{indent_str}scales={self.scales},\n'
+        repr_str += f'{indent_str}base_sizes={self.base_sizes},\n'
+        repr_str += f'{indent_str}scale_major={self.scale_major},\n'
+        repr_str += f'{indent_str}octave_base_scale='
+        repr_str += f'{self.octave_base_scale},\n'
+        repr_str += f'{indent_str}scales_per_octave='
+        repr_str += f'{self.scales_per_octave},\n'
+        repr_str += f'{indent_str}num_levels={self.num_levels}\n'
+        repr_str += f'{indent_str}centers={self.centers},\n'
+        repr_str += f'{indent_str}center_offset={self.center_offset})'
+        return repr_str
+
+
+class SSDAnchorGenerator(AnchorGenerator):
+    """
+    Anchor generator for SSD.
+
+    Args:
+        strides (list[int]  | list[tuple[int, int]]): Strides of anchors
+            in multiple feature levels.
+        ratios (list[float]): The list of ratios between the height and width
+            of anchors in a single level.
+        basesize_ratio_range (tuple(float)): Ratio range of anchors.
+        input_size (int): Size of feature map, 300 for SSD300,
+            512 for SSD512.
+        scale_major (bool): Whether to multiply scales first when generating
+            base anchors. If true, the anchors in the same row will have the
+            same scales. It is always set to be False in SSD.
+    """
+
+    def __init__(self,
+                 strides,
+                 ratios,
+                 basesize_ratio_range,
+                 input_size=300,
+                 scale_major=True):
+        assert len(strides) == len(ratios)
+        assert
